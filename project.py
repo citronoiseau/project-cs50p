cb1aed5724579ea18f78cf5ce27f496a46ad1022
Asia <134224668+citronoiseau@users.noreply.github.com>
2025-02-01 21:52:35 -0800

    added basic game loop and controls

--- project.py
+++ project.py
@@ -5,15 +5,16 @@ pygame.init()
 WIDTH = 500
 HEIGHT = 700
 tetrominos = [
-        [[1, 5, 9, 13], [4, 5, 6, 7]],
-        [[4, 5, 9, 10], [2, 6, 5, 9]],
-        [[6, 7, 9, 10], [1, 5, 6, 10]],
-        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]],
-        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]],
-        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
-        [[1, 2, 5, 6]],
+        [[1, 5, 9, 13], [4, 5, 6, 7]],#I
+        [[1, 2, 5, 6]], #O
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]], #T
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]], #J
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]], #L
+        [[6, 7, 9, 10], [1, 5, 6, 10]], #S
+        [[4, 5, 9, 10], [2, 6, 5, 9]], #Z
     ]
-COLORS = ["", "blue", "gray", "green", "lightblue", "orange", "purple", "red"]
+
+COLORS = ["", "blue",  "green", "lightblue", "orange", "purple", "red"]
 
 class Tetromino:
     def __init__(self, x, y):
@@ -37,17 +38,17 @@ class Tetris:
         self.score = 0
         self.tetromino = None
         self.cell_size = 30
+        self.offset_x = (WIDTH - self.columns * self.cell_size) // 2
+        self.offset_y = (HEIGHT - self.rows * self.cell_size) // 2
 
     def new_figure(self):
         self.tetromino = Tetromino(3, 0)    
     
     def draw_grid(self, screen):
-        offset_x = (WIDTH - self.columns * self.cell_size) // 2
-        offset_y = (HEIGHT - self.rows * self.cell_size) // 2
         for i in range(self.rows):  
             for j in range(self.columns):  
-                x = offset_x + j * self.cell_size
-                y = offset_y + i * self.cell_size
+                x = self.offset_x + j * self.cell_size
+                y = self.offset_y + i * self.cell_size
                 if self.grid[i][j] == 0:
                     pygame.draw.rect(screen, (40, 42, 54), (x, y, self.cell_size, self.cell_size), 1)
                 else:
@@ -55,13 +56,64 @@ class Tetris:
                     color_image = pygame.transform.scale(color_image, (self.cell_size, self.cell_size))
                     screen.blit(color_image, (x, y))
 
+    def draw_tetromino(self, screen):
+        if self.tetromino is not None:
+            for i in range(4):
+                for j in range(4):
+                    if i * 4 + j in self.tetromino.image():
+                        x = self.offset_x + (self.tetromino.x + j) * self.cell_size
+                        y = self.offset_y + (self.tetromino.y + i) * self.cell_size
+                        color_image = pygame.image.load(f'graphics/{COLORS[self.tetromino.color]}.png').convert_alpha()
+                        color_image = pygame.transform.scale(color_image, (self.cell_size, self.cell_size))
+                        screen.blit(color_image, (x, y))
+
+    def intersects(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.tetromino.image():
+                    if i + self.tetromino.y > self.rows - 1 or \
+                            j + self.tetromino.x > self.columns - 1 or \
+                            j + self.tetromino.x < 0 or \
+                            self.grid[i + self.tetromino.y][j + self.tetromino.x] > 0:
+                        return True
+        return False
+
+    def stop(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.tetromino.image():
+                    self.grid[i + self.tetromino.y][j + self.tetromino.x] = self.tetromino.color
+        self.clear_lines()
+        self.new_figure()
+        if self.intersects():
+            self.state = "gameover"
+
+    def clear_lines(self):
+        lines_to_clear = 0
+
+        for i in range(1, self.rows):
+            empty_cells = 0
+            for j in range(self.columns):
+                if self.grid[i][j] == 0:
+                    empty_cells += 1
+            if empty_cells == 0:
+                lines_to_clear += 1
+                for k in range(i, 1, -1):
+                    for j in range(self.columns):
+                        self.grid[k][j] = self.grid[k-1][j]
+        self.score += lines_to_clear ** 2 
+
+    def move_down(self):
+        self.tetromino.y += 1
+        if self.intersects():
+            self.tetromino.y -= 1
+            self.stop()  
 
     def update(self):
         if self.tetromino is None:
             self.new_figure()
-     
 
-            
+
 def main():
     screen = pygame.display.set_mode((500, 700))
     pygame.display.set_caption('Tetris')
@@ -70,21 +122,29 @@ def main():
     running = True
     tetris = Tetris(20, 10)
 
+
     #text
     font = pygame.font.Font('Font.ttf', 50)
-    game_message = font.render('Press space to start',False,(250,65,92))
-    game_message_rect = game_message.get_rect(center = (WIDTH // 2,200))
-
     #intro screen
-    game_name = font.render('TETRIS',False,(250,65,92))
+    game_name = font.render('TETRIS',False,(129,239,128))
     game_name_rect = game_name.get_rect(center = (WIDTH // 2,80))
     pic = pygame.image.load('graphics/intro.jpg').convert_alpha() 
     pic_scaled =  pygame.transform.scale(pic, (250, 250))
     pic_rec = pic_scaled.get_rect(center = (WIDTH // 2, 400))
+    game_message = font.render('Press space to start',False,(129,239,128))
+    game_message_rect = game_message.get_rect(center = (WIDTH // 2,200))
+
+
+    #gameover screen
+    game_name_over = font.render('TETRIS',False,(250,65,92))
+    game_name_over_rect = game_name.get_rect(center = (WIDTH // 2,80))
+    game_over_message = font.render('GAME OVER',False,(250,65,92))
+    game_over_message_rect = game_over_message.get_rect(center = (WIDTH // 2, 160))
+    restart_message = font.render('Press ESCAPE to restart',False,(250,65,92))
+    restart_message_rect = restart_message.get_rect(center = (WIDTH // 2, 300))
 
     #timer for tetrominos speed
-    obstacle_timer = pygame.USEREVENT + 1
-    pygame.time.set_timer(obstacle_timer, 2000)
+    tetromino_timer = pygame.USEREVENT + 1
 
     while running:
         for event in pygame.event.get():
@@ -92,14 +152,47 @@ def main():
                 running = False
             if tetris.state == "start" and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                     tetris.state = "active"
-       
-                                 
+                    pygame.time.set_timer(tetromino_timer, 500)
+            if tetris.state == "active":
+                if event.type == tetromino_timer:
+                    tetris.move_down()
+                if event.type == pygame.KEYDOWN:
+                    move_x = {pygame.K_a: -1, pygame.K_LEFT: -1, pygame.K_d: 1, pygame.K_RIGHT: 1}
+                    if event.key in move_x:
+                        previous = tetris.tetromino.x
+                        tetris.tetromino.x += move_x[event.key]
+                        if tetris.intersects():
+                            tetris.tetromino.x = previous
+                
+                    elif event.key in (pygame.K_w, pygame.K_UP):  # Rotate
+                        print("entered rotate")
+                        previous = tetris.tetromino.rotation
+                        tetris.tetromino.rotate()
+                        if tetris.intersects():
+                            tetris.tetromino.rotation = previous
+
+                    elif event.key == pygame.K_SPACE and tetris.tetromino:
+                        while not tetris.intersects():
+                            tetris.tetromino.y += 1
+                        tetris.tetromino.y -= 1
+                        tetris.stop()  
+                             
+            if tetris.state == "gameover":
+                pygame.time.set_timer(tetromino_timer, 0)   
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                        tetris.__init__(20,10)
+                        tetris.state = "start"
+           
+                          
         if tetris.state == "active":
-
             tetris.update()
-
-            screen.fill("purple")
+            screen.fill("black")
+            score = tetris.score
+            score_message = font.render(f'Score: {score}',False,(250,65,92))
+            score_message_rect = score_message.get_rect(center = (WIDTH // 2, 25))
+            screen.blit(score_message,score_message_rect)
             tetris.draw_grid(screen)
+            tetris.draw_tetromino(screen)
 
 
             pygame.display.flip()
@@ -110,6 +203,17 @@ def main():
             screen.blit(game_name,game_name_rect)
             screen.blit(game_message,game_message_rect)
             screen.blit(pic_scaled, pic_rec)
+            button.draw_button(screen)
+
+        if tetris.state == "gameover":
+            screen.fill((28, 28, 28))
+            screen.blit(game_name_over,game_name_over_rect)
+            screen.blit(game_over_message,game_over_message_rect)
+            score_message_rect = score_message.get_rect(center = (WIDTH // 2, 250))
+            screen.blit(score_message,score_message_rect)
+            screen.blit(restart_message, restart_message_rect)
+            
+
         
         pygame.display.update()
         clock.tick(60)  
